03c515b5f757c4cbdd18d32ba85f32d2a8a9f90e
leikeren <1690650740@qq.com>
2022-02-10 15:45:21 +0800

    feat:字符串旋转

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 方法1
+// function reverse(str) {
+//   return  Array.from(str).reverse().join('')
+// }
+
+// 方法2
+// function reverse(str) {
+//     let reversed = ''
+//     for(let char of str) {
+//         reversed = char + reversed 
+//     }
+//     return reversed
+// }
+
+// 方法3 
+// function reverse(str) {
+//     return str.split('').reduce((reversed, character)=> character + reversed, '')
+// }
+
+// 方法4
+function reverse(str) {
+    let tempArray = str.split('');
+    let reversedArray = [];
+    for(i= tempArray.length; i--; i > 0) {
+        reversedArray.push(tempArray[i]);
+    }
+    return reversedArray.join('') 
+}
 
 module.exports = reverse;
