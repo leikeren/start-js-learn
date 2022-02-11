b880090dbc485dd6776d3dc3cf3dc69eff9b3e62
leikeren <1690650740@qq.com>
2022-02-11 10:34:21 +0800

    ‘feat:buzz和chunk方法’

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,56 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+
+// 方法1
+// function chunk(array, size) {
+//     // 可分割的数组量
+//     const arrayLength = Math.ceil(array.length / size);
+//     const chunkArray = [];
+//     for(let i =0; i< arrayLength; i++) {
+//         chunkArray.push(array.slice(i*size, (i+1)*size))
+//     }
+//     return chunkArray;
+// }
+
+// 方法2
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let element of array) {
+//         // [1]
+//         // [1,2]
+//         // [3]
+//         // [3,4]
+//         const last = chunked[chunked.length - 1];
+
+//         if(!last || last.length === size) {
+//             /**
+//              * [[1]]
+//              * [[1,2],[3]]
+//              */
+//             chunked.push([element]);
+//         } else {
+//             // [1,2]
+//             // [3,4]
+//             last.push(element)
+//         }
+//     }
+
+//     return chunked;
+// }
+
+// 方法3 另外一种使用slice的方式
+function chunk (array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size))
+        index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
