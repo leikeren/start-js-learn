7425d9abb6728e13c923affe2f5a5bd9b70645b5
leikeren <1690650740@qq.com>
2022-02-14 15:48:08 +0800

    feat:使用正则匹配

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// 方法1
+// function vowels(str) {
+//     let count = 0;
+//     const vowArray = ['a', 'e', 'i', 'o', 'u'];
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowArray.includes(str[i].toLowerCase())) {
+//             count = count + 1;
+//         }
+//     }
+
+//     return count;
+// }
+
+// 方法2
+function vowels(str) {
+  const matches =   str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
