7c7e95093e3719d0e8baf7ba1c3139c25e8006ab
leikeren <1690650740@qq.com>
2022-02-14 15:19:58 +0800

    feat:递归金字塔打印

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n -1) / 2);
+
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+
+//         for (let column = 0; column < 2 * n -1; column++) {
+//             if(midpoint - row <= column && midpoint + row >= column ) {
+//                 level += '#'
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+
+//         console.log(level)
+//     }
+// }
+
+function pyramid(n, row = 0, level = '' ) {
+    const midpoint = Math.floor((2 * n -1) / 2);
+    // n 和行数相等 return
+    if(n === row) {
+        return;
+    }
+    // 如果打印的stair和2n-1等差数列相同
+    if(level.length === (2 *n - 1)) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    let add;
+    if(midpoint - row <= level.length && midpoint + row >= level.length ) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
